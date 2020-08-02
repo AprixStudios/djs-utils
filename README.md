bc166de94434a8fed09ba6c3744d9d36628c853a
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-08-02 13:47:02 +0200

    Creation

--- README.md
+++ README.md
@@ -1,2 +1,149 @@
-# djs-utils
- Some handy utilities for Discord.JS bots
+<h1 align="center">Welcome to djs-utils 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/AprixStudios/DJS-Utils#readme" target="_blank">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
+  </a>
+  <a href="https://github.com/AprixStudios/DJS-Utils/graphs/commit-activity" target="_blank">
+    <img alt="Maintenance" src="https://img.shields.io/badge/Maintained%3F-yes-green.svg" />
+  </a>
+  <a href="https://github.com/AprixStudios/DJS-Utils/blob/master/LICENSE" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/github/license/AprixStudios/djs-utils" />
+  </a>
+  <a href="https://twitter.com/Aprixiaa" target="_blank">
+    <img alt="Twitter: Aprixiaa" src="https://img.shields.io/twitter/follow/Aprixiaa.svg?style=social" />
+  </a>
+</p>
+
+> Some handy utilities for discord.js bots
+
+### 🏠 [Homepage](https://github.com/AprixStudios/DJS-Utils#readme)
+
+## Install
+
+```sh
+npm install djs-utils
+```
+
+## Docs
+
+#### Initialization:
+```js
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const djsUtils = require('djs-utils');
+const utils = new djsUtils.Utils(client);
+```
+
+
+#### getUser
+
+Get a Discord User securily using mention, name, or id.
+
+| Parameters | Type | Optional | Description | Example |
+| --- | --- | --- | --- | --- |
+| mention | < User Mention, String > | No  | Preferably pass an argument from your arguments array. | `args[0]` |
+
+**Returns:** UserObject
+
+#### getRoles
+
+Get a Discord Roles securily using mention, name, or id.
+
+| Parameters | Type | Optional | Description | Example |
+| --- | --- | --- | --- | --- |
+| mention | < Role Mention, String > | No  | Preferably pass an argument from your arguments array. | `args[1]` |
+| roles | < RoleManager > | No  | Must pass `guild.roles` | `guild.roles` |
+
+**Returns:** RoleObject
+
+#### getChannels
+
+Get a Discord Channel securily using mention, name, or id.
+
+| Parameters | Type | Optional | Description | Example |
+| --- | --- | --- | --- | --- |
+| mention | < Channel Mention, String > | No  | Preferably pass an argument from your arguments array. | `args[0]` |
+| channels | < GuildChannelManager> | No  | Must pass `guild.channels` | `guild.channels` |
+
+**Returns:** ChannelObject
+
+#### getTime
+
+Get a sufficient time, from ms, in a format that wont trigger OCD.
+
+| Parameters | Type | Optional | Description | Example |
+| --- | --- | --- | --- | --- |
+| s   | < Number > | No  | Must be a number, as it will calculate the time from the millisecond input in this parameter. Advice: Use `endtime-Date.now()` | `res.endTime-Date.now()` |
+
+**Returns:** String
+
+#### setTime
+
+Set a sufficient endtime with the choices `s`, `m`, `h`, `d`, `w`, which you can work our what does.
+
+| Parameters | Type | Optonal | Description | Example |
+| --- | --- | --- | --- | --- |
+| time | < String > | No  | Must be a string as it uses both numbers and variables in the same parameter. | `args[1]` |
+
+**Returns:** Number (milliseconds)
+
+#### getPages
+
+Easily get pages for your large lists.
+
+| Parameters | Type | Optional | Description | Example |
+| --- | --- | --- | --- | --- |
+| fullArr | < Array > | No  | The array of which to make the pages from. | `['apple', 'pineapple', 'orange', 'grape', 'melon', 'avocado']` |
+| pageNum | < Number > | No  | The page requested. | `1` |
+| amountPerPage | < Number > | Yes | The amount of items per page. Please keep this consistent for the people with OCD. `default: 5` | `10` |
+
+**Returns:** Object: {pages: pages, amount: \`\${pageNum}/\${pagesAmount}\`}
+
+#### setCleanTitle & setCleanFooter
+
+Check if a message ends with `-c` or `-clean`, if it does, this will make it not make a title/footer, if it does not, it will create the title/footer.
+
+| Parameters | Type | Optional | Description | Example |
+| --- | --- | --- | --- | --- |
+| message | < MessageObject > | No  | The message object, of which it checks the content for. | `message` |
+| embed | < MessageEmbed > | No  | The embed of which to make clean/not clean. | `embed` |
+| title/footer | < String > | No  | The title/footer to put in the embed if it's not clean. | `Banned!` |
+
+**Returns:** Promise: It sets the values in the embed.
+
+#### sendSafeEmbed
+
+In case the bot doesn't have permissions to send embeds, this function will try sending it. If it can't, it will send it as a normal message.
+
+| Parameters | Type | Optional | Description | Example |
+| --- | --- | --- | --- | --- |
+| embed | < MessageEmbed > | No  | The embed of which we're trying to send. | `embed` |
+| channel | < ChannelObject > | No  | The channel of where we will send the embed/message. | `channel` |
+
+**Returns:** Promise: It sends the embed and sends it as a message if no perms for embeds.
+
+## Author
+
+👤 **AprixStudios**
+
+* Website: https://aprixstudios.xyz/
+* Twitter: [@Aprixiaa](https://twitter.com/Aprixiaa)
+* Github: [@AprixStudios](https://github.com/AprixStudios)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/AprixStudios/DJS-Utils/issues). 
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+<a href="https://www.patreon.com/Aprixia">
+  <img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160">
+</a>
+
+## 📝 License
+
+Copyright © 2020 [AprixStudios](https://github.com/AprixStudios).<br />
+This project is [MIT](https://github.com/AprixStudios/DJS-Utils/blob/master/LICENSE) licensed.
---
+++ index.js
@@ -0,0 +1,287 @@
+const Discord = require('discord.js');
+
+class Utils {
+    constructor (client) {
+        if (!client) {
+            throw new Error(`client is required, but isn't given.`);
+        }
+        if (!client.user) {
+            throw new Error(`client is not valid.`);
+        }
+        this.client = client;
+    }
+    
+    // Get User Function
+    async getUser(mention) {
+        if (!mention) {
+            throw new SyntaxError(`mention is required, but isn't defined.`);
+        }
+        if (typeof mention !== "string") {
+            throw new TypeError(`mention is not a string.`);
+        }
+        return new Promise(resolve => {
+            if (!mention) return resolve();
+            if (mention.startsWith('<@') && mention.endsWith('>')) {
+                // if it is a mention it will do this
+                mention = mention.slice(2, -1);
+                if (mention.startsWith('!')) {
+                    mention = mention.slice(1);
+                }
+                return resolve(this.client.users.cache.get(mention));
+            }
+            else if (this.client.users.cache.get(mention)) {
+                // if it is a id it will do this
+                return resolve(this.client.users.cache.get(mention));
+            } else {
+                // if it isn't either of them it will do this
+                if (this.client.users.cache.find(u => u.tag.toLowerCase().startsWith(mention.toLowerCase()))) {
+                    // if it can find a user from the input it will do this
+                    return resolve(this.client.users.cache.find(u => u.tag.toLowerCase().startsWith(mention.toLowerCase())));
+                }
+                else {
+                    // if not it wont do anything
+                    return resolve(null);
+                }
+            }
+        });
+    }
+
+    // Get Role Function
+    async getRole(mention, roles) {
+        if (!mention) {
+            throw new SyntaxError(`mention is required, but isn't defined.`);
+        }
+        if (typeof mention !== "string") {
+            throw new TypeError(`mention is not a string.`);
+        }
+        if (!roles) {
+            throw new SyntaxError(`roles is required, but isn't defined.`);
+        }
+        if (!roles.cache) {
+            throw new TypeError(`roles is not a role list.`);
+        }
+        return new Promise(resolve => {
+            if (!mention) return resolve();
+            if (mention.startsWith('<&') && mention.endsWith('>')) {
+                // if it is a mention it will do this
+                mention = mention.slice(2, -1);
+                if (mention.startsWith('!')) {
+                    mention = mention.slice(1);
+                }
+                return resolve(roles.cache.get(mention));
+            }
+            else if (roles.cache.get(mention)) {
+                // if it is a id it will do this
+                return resolve(roles.cache.get(mention));
+            } else {
+                // if it isn't either of them it will do this
+                if (roles.cache.find(r => r.name.toLowerCase().startsWith(mention.toLowerCase()))) {
+                    // if it can find a role from the input it will do this
+                    return resolve(roles.cache.find(r => r.name.toLowerCase().startsWith(mention.toLowerCase())));
+                }
+                else {
+                    // if not it wont do anything
+                    return resolve(null);
+                }
+            }
+        });
+    }
+
+    // Get Channel Function
+    async getChannel(mention, channels) {
+        if (!mention) {
+            throw new SyntaxError(`mention is required, but isn't defined.`);
+        }
+        if (typeof mention !== "string") {
+            throw new TypeError(`mention is not a string.`);
+        }
+        if (!channels) {
+            throw new SyntaxError(`channels is required, but isn't defined.`);
+        }
+        if (!channel.cache) {
+            throw new TypeError(`channels is not a channel list.`);
+        }
+        return new Promise(resolve => {
+            if (!mention) return resolve();
+            if (mention.startsWith('<#') && mention.endsWith('>')) {
+                // if it is a mention it will do this
+                mention = mention.slice(2, -1);
+                if (mention.startsWith('!')) {
+                    mention = mention.slice(1);
+                }
+                return resolve(channels.cache.get(mention));
+            }
+            else if (channels.cache.get(mention)) {
+                // if it is a id it will do this
+                return resolve(channels.cache.get(mention));
+            } else {
+                // if it isn't either of them it will do this
+                if (channels.cache.find(c => c.name.toLowerCase().startsWith(mention.toLowerCase()))) {
+                    // if it can find a channel from the input it will do this
+                    return resolve(channels.cache.find(c => c.tag.toLowerCase().startsWith(mention.toLowerCase())));
+                }
+                else {
+                    // if not it wont do anything
+                    return resolve(null);
+                }
+            }
+        });
+    }
+
+    // Get Time Function
+    async getTime(s) {
+        if (!s) {
+            throw new SyntaxError(`s is required, but isn't defined.`);
+        }
+        if (typeof s !== "number") {
+            throw new TypeError(`s is not a number.`);
+        }
+        // make the variables
+        let ms = s % 1000;
+        s = (s - ms) / 1000;
+        let secs = s % 60;
+        s = (s - secs) / 60;
+        let mins = s % 60;
+        s = (s - mins) / 60
+        let hours = s % 24;
+        let days = (s - hours) / 24;
+    
+        // put them together
+        // it works I swear
+        let displayTime;
+        if (secs >= 10) displayTime = secs;
+        else if (secs <= 9) displayTime = `0${secs}`;
+
+        if (mins >= 10) displayTime = `${mins}:${displayTime}`;
+        else if (mins <= 9) displayTime = `0${mins}:${displayTime}`;
+
+        if (hours >= 10) displayTime = `${hours}:${displayTime}`;
+        else if (hours <= 9) displayTime = `0${hours}:${displayTime}`;
+
+        if (days > 0) displayTime = `${days}:${displayTime}`;
+
+        return displayTime;
+
+        //return `${hours}:${mins}:${secs}`;
+    }
+
+    async setTime(time) {
+        if (!time) {
+            throw new SyntaxError(`time is required, but isn't defined.`);
+        }
+        if (typeof time !== "string") {
+            throw new TypeError(`time is not a string.`);
+        }
+        let times = ['s', 'm', 'h', 'd', 'w'];
+        return new Promise(resolve => {
+            if (!time || !times.some(letter => time.toLowerCase().endsWith(letter)) || isNaN(time.slice(0,-1))) {
+                return resolve(null);
+            } else if (times.some(letter => time.toLowerCase().endsWith(letter)) && !isNaN(time.slice(0,-1))) {
+                let timeInd;
+                let timeAt = time.slice(-1);
+                if (timeAt === 's') timeInd = 1000;
+                if (timeAt === 'm') timeInd = 60000;
+                if (timeAt === 'h') timeInd = 3600000;
+                if (timeAt === 'd') timeInd = 86400000;
+                if (timeAt === 'w') timeInd = 86400000*7;
+                let timeMs = time.slice(0,-1);
+                let timeMsAdd = timeMs*timeInd;
+                let timeMS = Date.now()+timeMsAdd;
+                return resolve(timeMS);
+            }
+        });
+    }
+
+    async getPages(fullArr, pageNum, amountPerPage) {
+        if (!fullArr) {
+            throw new SyntaxError(`fullArr is required, but isn't defined.`);
+        }
+        if (typeof fullArr !== "array") {
+            throw new TypeError(`fullArr isn't an array.`);
+        }
+        if (!pageNum) {
+            throw new SyntaxError(`pageNum is required, but isn't defined.`);
+        }
+        if (typeof pageNum !== "number") {
+            throw new TypeError(`pageNum is not a number.`);
+        }
+        if (!amountPerPage) amountPerPage = 5;
+        let multiNum = Math.ceil(pageNum)-1;
+        if (mutliNum < 0) multiNum = 0;
+        let startNum = multiNum*amountPerPage;
+        let pages = Math.floor(fullArr.length/5) >= multiNum ? fullArr.slice(startNum,startNum+amountPerPage) : fullArr.slice(0,amountPerPage);
+        await pages;
+        let pagesAmount = Math.ceil(fullArr.length/amountPerPage);
+        let pagesObj = {pages: pages, amount: `${pageNum}/${pagesAmount}`};
+        return pagesObj;
+    }
+
+    async setCleanTitle(message, embed, title) {
+        if (!message) {
+            throw new SyntaxError(`message is required, but isn't defined.`);
+        }
+        if (!message.content || typeof message.content !== "string") {
+            throw new TypeError(`message isn't a message object.`);
+        }
+        if (!embed) {
+            throw new SyntaxError(`embed is required, but isn't defined.`);
+        }
+        if (!embed.type) {
+            throw new TypeError(`embed isn't a MessageEmbed object.`);
+        }
+        if (!title) {
+            throw new SyntaxError(`title is required, but isn't defined.`);
+        }
+        if (typeof title !== "string") {
+            throw new TypeError(`title isn't a string.`);
+        }
+        if (!message.content.toLowerCase().endsWith(` -c`) && !message.content.toLowerCase().endsWith(` -clean`)) embed.setTitle(title);
+    }
+
+    async setCleanFooter(message, embed, footer) {
+        if (!message) {
+            throw new SyntaxError(`message is required, but isn't defined.`);
+        }
+        if (!message.content || typeof message.content !== "string") {
+            throw new TypeError(`message isn't a message object.`);
+        }
+        if (!embed) {
+            throw new SyntaxError(`embed is required, but isn't defined.`);
+        }
+        if (!embed.type) {
+            throw new TypeError(`embed isn't a MessageEmbed object.`);
+        }
+        if (!title) {
+            throw new SyntaxError(`title is required, but isn't defined.`);
+        }
+        if (typeof title !== "string") {
+            throw new TypeError(`title isn't a string.`);
+        }
+        if (!message.content.toLowerCase().endsWith(` -c`) && !message.content.toLowerCase().endsWith(` -clean`)) embed.setFooter(footer);
+    }
+
+    async sendSafeEmbed(embed, channel) {
+        if (!embed) {
+            throw new SyntaxError(`embed is required, but isn't defined.`);
+        }
+        if (!embed.type) {
+            throw new TypeError(`embed isn't a MessageEmbed object.`);
+        }
+        if (!channel) {
+            throw new SyntaxError(`channel is required, but isn't defined.`);
+        }
+        if (!channel.type) {
+            throw new TypeError(`channel isn't a channel object.`)
+        }
+        channel.send(embed).catch(err => {
+            let fieldsToMsg = [];
+            for (i=0;i<embed.fields.length;i++) {
+                fieldsToMsg.push(`**${embed.fields[i].name}**: ${embed.fields[i].value}`);
+            }
+            let msgToSend = `${embed.author.name ? `${embed.author.name}\n` : ""}${embed.title ? `**${embed.title}**\n` : ""}${embed.description ? `${embed.description}\n` : ""}${fieldsToMsg.join('\n')}\n${embed.footer ? `\n${embed.footer}` : ""}`;
+            channel.send(msgToSend).catch(error => {throw new Error(`Error from Discord.JS: ${error}`)});
+        });
+    }
+}
+
+module.exports = { Utils }
